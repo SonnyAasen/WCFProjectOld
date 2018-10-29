e7a1bb592afc0b8d9b925afd788af6d3db05d84e
SonnyAasen <35238145+SonnyAasen@users.noreply.github.com>
2018-10-29 06:43:34 +0100

    Updated til Appconfig part

---
+++ CarRentalService/ICarRentalWCFService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CarRentalService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICarRentalService" in both code and config file together.
+    [ServiceContract]
+    public interface ICarRentalService
+    {
+        [OperationContract]
+        List<Car> GetAllCars();
+    }
+}
---
+++ CarRentalWCFService/CarRentalWCFService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using BL;
+
+namespace CarRentalWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CarRentalWCFService" in both code and config file together.
+    public class CarRentalWCFService : ICarRentalWCFService
+    {
+        public void RemoveCar(string RegNr)
+        {
+            var carService = new CarService();
+            carService.Remove(RegNr);
+        }
+    }
+}
